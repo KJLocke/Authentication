e340c3a35e5c387d3953b7f44fee87d71978dd59
Kenton <kenton@ikenect.com>
2017-06-10 08:52:14 +0100

    User mpdel created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+
   root 'static_page#index'
+  get '/help',    to: 'static_page#help'
+  resources :users
 
-get '/help',    to: 'static_page#help'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
